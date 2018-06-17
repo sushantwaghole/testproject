a17d7870811aa4f1e9261dd24079bb3e6d0b11aa
sk <sushantwaghole@gmail.com>
2018-06-17 02:38:28 -0700

    status
    Signed-off-by: sk <sushantwaghole@gmail.com>

---
+++ testproject/src/sk/firstprogram.java
@@ -0,0 +1,10 @@
+package sk;
+
+public class firstprogram {
+
+	public static void main(String[] args) {
+		System.out.println("this is first program of commit and push with the help of git hub");
+
+	}
+
+}
